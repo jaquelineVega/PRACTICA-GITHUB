bd82e726aabcefcd475e9dfa12d65d1b49bfb169
Erick Garcia <agratobgs@gmail.com>
2019-12-03 21:12:55 -0600

    Creacion de Navbar y Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { CardComponent } from './Components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
