a6faf34a57e2d550e3f304711a7382d37d939c23
jaquelineVega <55595112+jaquelineVega@users.noreply.github.com>
2019-12-05 23:31:34 -0600

    Se agrega pnotify

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import PNotify from 'pnotify/dist/es/PNotify';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
+
 
 @Component({
   selector: 'app-home',
@@ -7,8 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
-
+  constructor() {
+    PNotifyButtons; // Initiate the module. Important!
+    PNotify.alert('Notice me, senpai!');
   }
 
   ngOnInit() {
